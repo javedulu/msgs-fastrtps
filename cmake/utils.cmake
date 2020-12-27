fba1860f5eca7fa3f7815cfd083f5730d598f212
Javed Shaik <javedulu@gmail.com>
2020-12-27 12:49:23 -0800

    Update utils.cmake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -15,48 +15,6 @@ MACRO(SUBDIRLIST RESULT CURDIR)
   SET(${RESULT} ${dirlist})
 ENDMACRO()
 #-------------------------------------------------------------------------------
-MACRO(ADD_REMOTE_EXECUTABLE exe_name)
-
-    SET(REMOTE_PLATFORM "linux")
-    SET(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/deps/ssh2exec/cmake")
-
-    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR})
-    INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/deps/ssh2exec)
-    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR})
-
-    FIND_PACKAGE(SSH2 REQUIRED)
-
-    SET(REMOTE_PLATFORM_ARCH ${REMOTE_PLATFORM}_${ARCH})
-
-    SET(TARGET_HEADERS
-        #${CMAKE_SOURCE_DIR}/deps/ssh2exec/libssh2.hxx
-        #${CMAKE_BINARY_DIR}/sshxconfig.h
-    )
-
-    # configure a header file to pass some of the CMake settings
-    # to the source code
-    configure_file (
-        "${CMAKE_SOURCE_DIR}/sshxs/remotexec.cxx.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/${EXE_NAME}-remotexec.cxx"
-    )
-
-    SET(TARGET_SOURCES
-        "${CMAKE_CURRENT_BINARY_DIR}/${EXE_NAME}-remotexec.cxx"
-    )
-
-    ADD_EXECUTABLE(${EXE_NAME}-remote 
-       ${TARGET_SOURCES}
-       ${TARGET_HEADERS}
-    )
-
-    TARGET_LINK_LIBRARIES(${EXE_NAME}-remote ${LIBSSH2_LIBRARY})
-
-    SET_TARGET_PROPERTIES(${EXE_NAME}-remote PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-
-    SET_TARGET_PROPERTIES(${EXE_NAME}-remote  PROPERTIES FOLDER sshxs)
-    
-ENDMACRO()
-#-------------------------------------------------------------------------------
 MACRO (ADD_PLATFORM_ARCH)
     set(ARCH "x32")
     IF (${CMAKE_SIZEOF_VOID_P} EQUAL 8)
@@ -84,31 +42,3 @@ MACRO (ADD_PLATFORM_ARCH)
        set(PLATFORM "linux")
     ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 ENDMACRO()
-
-
-#IF (APPLE)
-#    SET(REMOTE_BUILD "YES" CACHE BOOL "REMOTE BUILD FROM OSX to LINUX")
-#ENDIF()
-#
-#IF(REMOTE_BUILD)
-##TODO:
-## 1. rsync the build directory to ros-linux
-## 2. cd to build directory on ros-linux 
-## 3. make on build direcotry on ros-linux
-#set(REMOTE_URL ros@SJOLAP037.esi-internal.esi-group.com)
-##set(REMOTE_URL ros@localhost)
-#string(TIMESTAMP TODAY "%Y%m%d")
-#set(REMOTE_DIR /tmp/${PROJECT_NAME}-${TODAY})
-#
-#ADD_CUSTOM_COMMAND(OUTPUT ${TARGET_NAME}Make 
-#    COMMAND rsync -arvzh --exclude={xbuild/} ${CMAKE_SOURCE_DIR}/ ${REMOTE_URL}:${REMOTE_DIR}
-#    COMMAND ssh -t ${REMOTE_URL} \"mkdir -p ${REMOTE_DIR}/build && cd ${REMOTE_DIR}/build && cmake .. -DREMOTE_BUILD:BOOL=NO\"
-#    COMMAND ssh -t ${REMOTE_URL} \"cd ${REMOTE_DIR}/build && make\"
-#    DEPENDS ${TARGET_NAME}
-#    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-#    COMMENT "BUILDING ASSIMPLOADER ... "
-#)
-#
-#ADD_CUSTOM_TARGET(${TARGET_NAME}Build ALL DEPENDS ${TARGET_NAME}Make)
-#
-#ENDIF(REMOTE_BUILD)
